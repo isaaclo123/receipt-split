845afc9f2a86912335d5ba872c9aa9eb5e3020ab
isaaclo123 <isaaclo123@gmail.com>
2019-12-17 12:30:38 -0600

    try to deploy

---
+++ build/precache-manifest.2a52c28bc9a0ff292530c46867907baa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8077e6e60a867bd33bff3cd26095c43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef56957df33e7ec9f70e",
+    "url": "/static/css/main.53289edc.chunk.css"
+  },
+  {
+    "revision": "6fb123f3bfe973f7a702",
+    "url": "/static/js/2.fc218dcd.chunk.js"
+  },
+  {
+    "revision": "ef56957df33e7ec9f70e",
+    "url": "/static/js/main.31bc60f9.chunk.js"
+  },
+  {
+    "revision": "fdf85edc06e96ac13219",
+    "url": "/static/js/runtime-main.56563e62.js"
+  }
+]);
